c63fe3879c3c9f1882e9c7270648f3c67bac3780
lmacwan <lmacwan@hotmail.com>
2016-08-30 09:57:31 +0530

    Intermididate commit before pushing to GitHub

---
+++ HtmlParser.DTO/DTO/Match/MatchListItemDTO.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlParser.DTO.Match
+{
+    public class MatchListItemDTO
+    {
+        [JsonProperty("unique_id")]
+        public string Id { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("title")]
+        public string Name { get; set; }
+    }
+
+    public class RootObject
+    {
+        [JsonProperty("data")]
+        public List<MatchListItemDTO> Items { get; set; }
+    }
+}
